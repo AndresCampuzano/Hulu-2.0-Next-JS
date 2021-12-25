da15cbd8a5c025c2ea705ae36dc6ae733bd8636b
Andres Campuzano <acampuzano@hugeinc.com>
2021-12-24 21:17:27 -0500

    add Nav component and done UI

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { HeaderItem } from "./HeaderItem";
+import { Nav } from "./Nav";
 import {
   BadgeCheckIcon,
   CollectionIcon,
@@ -11,21 +12,24 @@ import {
 
 export const Header = () => {
   return (
-    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
-      <div className="flex flex-grow justify-evenly max-w-2xl ">
-        <HeaderItem title="HOME" icon={HomeIcon} />
-        <HeaderItem title="TRENDING" icon={LightningBoltIcon} />
-        <HeaderItem title="VERIFIED" icon={BadgeCheckIcon} />
-        <HeaderItem title="COLLECTIONS" icon={CollectionIcon} />
-        <HeaderItem title="SEARCH" icon={SearchIcon} />
-        <HeaderItem title="ACCOUNT" icon={UserIcon} />
-      </div>
-      <Image
-        className="object-contain"
-        src="https://links.papareact.com/ua6"
-        width={200}
-        height={100}
-      />
-    </header>
+    <>
+      <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+        <div className="flex flex-grow justify-evenly max-w-2xl ">
+          <HeaderItem title="HOME" icon={HomeIcon} />
+          <HeaderItem title="TRENDING" icon={LightningBoltIcon} />
+          <HeaderItem title="VERIFIED" icon={BadgeCheckIcon} />
+          <HeaderItem title="COLLECTIONS" icon={CollectionIcon} />
+          <HeaderItem title="SEARCH" icon={SearchIcon} />
+          <HeaderItem title="ACCOUNT" icon={UserIcon} />
+        </div>
+        <Image
+          className="object-contain"
+          src="https://links.papareact.com/ua6"
+          width={200}
+          height={100}
+        />
+      </header>
+      <Nav />
+    </>
   );
 };
