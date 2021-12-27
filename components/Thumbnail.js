79166523569c7ab0048e418cadde173f48b9fa45
Andres Campuzano <acampuzano@hugeinc.com>
2021-12-27 17:59:59 -0500

    add react-lazyload dependency

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import { ThumbUpIcon } from "@heroicons/react/outline";
+import LazyLoad from "react-lazyload";
 
 // eslint-disable-next-line react/display-name
 export const Thumbnail = ({ result }) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
   return (
-    <div
+    <LazyLoad
       className="p-2 group cursor-pointer transition
       duration-200 ease-in transform
       sm:hover:scale-105 hover:z-50"
@@ -33,6 +34,6 @@ export const Thumbnail = ({ result }) => {
           <ThumbUpIcon className="h-5 mx-2" /> {result.vote_count}
         </p>
       </div>
-    </div>
+    </LazyLoad>
   );
 };
